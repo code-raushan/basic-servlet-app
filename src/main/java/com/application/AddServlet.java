5fcb9377a096738abcdd8572911f733c33db09dd
Raushan Kumar <raushan.development@gmail.com>
2023-09-20 17:13:55 +0530

    Session Management added with Cookie

--- src/main/java/com/application/AddServlet.java
+++ src/main/java/com/application/AddServlet.java
@@ -2,11 +2,13 @@ package com.application;
 
 import java.io.IOException;
 
-import javax.servlet.RequestDispatcher;
+//import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class AddServlet extends HttpServlet {
 	/**
@@ -20,10 +22,21 @@ public class AddServlet extends HttpServlet {
 		
 		int k = i + j;
 		
-		req.setAttribute("k", k);
+//		req.setAttribute("k", k);
 		
-		RequestDispatcher rd = req.getRequestDispatcher("square");
-		rd.forward(req, res);
+//		RequestDispatcher rd = req.getRequestDispatcher("square");
+//		rd.forward(req, res);
+		
+		int x = k;
+		
+		Cookie cookie = new Cookie("x", x+"");
+		res.addCookie(cookie);
+		
+		HttpSession session = req.getSession();
+		
+		session.setAttribute("k", k);
+		
+		res.sendRedirect("square"); // redirecting to the square servlet
 		
 	}
 	
--- src/main/java/com/application/SquareServlet.java
+++ src/main/java/com/application/SquareServlet.java
@@ -3,18 +3,35 @@ package com.application;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
 
 public class SquareServlet extends HttpServlet {
 	public void doGet(HttpServletRequest req, HttpServletResponse res)throws IOException{
-		int k =(int) req.getAttribute("k");
 		
-		k = k * k;
+//		int k =(int) req.getAttribute("k");
+//		k = k * k;
+		
+		HttpSession session = req.getSession();
+		int k = (int) session.getAttribute("k");
+		k = k*k;
+		
+		Cookie[] cookies = req.getCookies();
+		int x=0;
+		for(Cookie c: cookies) {
+			if(c.getName().equals("x")) {
+				x = Integer.parseInt(c.getValue());
+				x = x*x;
+			}
+		}
 		
 		PrintWriter out = res.getWriter();
-		out.println("The squared result is "+k);
+		out.println("The squared result is "+k + " from the computation from session variable");
+		out.println("The squared result is "+x + " from the computation from cookie variable");
+
 		
 	}
 
